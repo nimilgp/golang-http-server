8ec1f768c10877accf39b37b7a88cd69fc97dd71
nimil <nimilgeorgepradeepngp@gmail.com>
2024-03-24 08:33:19 +0530

    added loop for accept

--- app/server.go
+++ app/server.go
@@ -13,11 +13,13 @@ func main() {
 		os.Exit(1)
 	}
 	
-	conn, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			conn.Close()
+			break
+		}
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	conn.close()
 }
